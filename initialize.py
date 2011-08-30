c9d7ec87ef4e58b638d30fe2fca3ae17ce1dc3c3
Dan Schultz <slifty@gmail.com>
2011-08-30 16:27:38 -0400

    corrected an initialization bug

--- initialize.py
+++ initialize.py
@@ -1,8 +1,5 @@
 import os
 from luminoso.study import StudyDirectory;
 
-claim_dir = "claims"
-if not os.path.exists(claim_dir):
-    os.makedirs(claim_dir)
-
+claim_dir = "claims/"
 StudyDirectory.make_new(claim_dir)
